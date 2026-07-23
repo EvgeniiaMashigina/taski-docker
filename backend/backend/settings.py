61a0d0f29ddd647823ab2f0d59b88edba44397ce
Evgeniia Mashigina <mashigina00@mail.ru>
2026-07-23 20:12:39 +0300

    new port3

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -118,3 +118,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ORIGIN_WHITELIST = [
     'http://localhost:3000'
 ]
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://pumpumpum.3utilities.com",
+]
